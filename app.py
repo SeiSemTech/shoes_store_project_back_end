8b1161dfc877062407125836006342498dcb38ea
Jose García <jose.garcia@rappi.com>
2021-04-28 13:54:11 -0500

    Solve CORS

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import app_auth, app_user
 from database import mysql
 
 app = FastAPI()
+
+origins = ['https://zapacommerce.web.app']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(app_auth, prefix='/api')
 app.include_router(app_user, prefix='/api')
