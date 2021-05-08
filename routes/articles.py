cd21027e89b49f435ffab0e94e397d87c32a2b85
equaltech2021@gmail.com <equaltech2021@gmail.com>
2021-05-08 17:02:52 -0500

    TASK 129
    Creación de lógica en BackEnd para listar productos, primera versión a falta de revisiones. Se hace camvio en el __init__

--- routes/articles.py
+++ routes/articles.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from database.mysql import execute_query
 from starlette.status import HTTP_409_CONFLICT, HTTP_201_CREATED
 from fastapi.responses import JSONResponse
-from interfaces.article import *
+from interfaces.articles import Product
 
 app_article = APIRouter()
 
@@ -12,3 +12,32 @@ app_article = APIRouter()
     tags=['Article'],
     summary="Create article in SQL database"
 )
+
+
+
+
+@app_article.get(
+    path='/read',
+    status_code=200,
+    tags=['Article'],
+    summary="Read articles in SQL database"
+)
+async def getAllArticles():
+
+    data = execute_query(
+        query_name="get_all_articles.sql",
+        fetch_data=True,
+        **request.dict()
+    )
+
+    if len(data) > 0:
+        return {
+            "articles": data  # TODO RETURN TOKEN
+        }
+    else:
+        return HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail="No articles have been published"
+        )
+
+    return { data }
