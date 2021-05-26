7d97c1bb90a71d5c22427190c24bd6176dfcc25b
danielsan89-lang <83325673+danielsan89-lang@users.noreply.github.com>
2021-05-26 18:31:46 -0500

    Corrección Llave Primaria
    Se corrigio el nombre de la llave primaria en la tabla Bill

--- context/migrate.sql
+++ context/migrate.sql
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXISTS bill (
   date date NOT NULL,
   total_quantity int(11) NOT NULL,
   total_price int(11) NOT NULL,
-  UNIQUE KEY (id_bill)
+  UNIQUE KEY (id)
 );
 --
 -- Índices para tablas volcadas
